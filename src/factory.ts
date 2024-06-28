b1be6100aa6d7b394a528a635fd7f671eeb58a6d
Tuhin Samanta <43666568+tuhin-samanta@users.noreply.github.com>
2024-06-29 02:29:13 +0530

    (feat): Made factory working

--- src/factory.ts
+++ src/factory.ts
@@ -5,4 +5,13 @@
  * @param {number} [step] - The step to increment each number in the sequence. Default is 1.
  * @returns {Function} - A function that generates the number sequence every time it is called.
  */
-export const factory = (start?: number, step?: number) => {};
+export const factory = (start?: number, step?: number) => {
+  start = !isNaN(start as number) ? start : 0;
+  step = !isNaN(step as number) ? step : 1;
+
+  let current: number = Number(start);
+  return () => {
+    current += Number(step);
+    return current;
+  };
+};
--- src/main.ts
+++ src/main.ts
@@ -1,27 +1,39 @@
-import { factory } from "./factory";
+import { factory } from './factory';
 
 let count = factory(0, 1);
 
-function update_count_and_reset_counter() {}
+function update_count_and_reset_counter() {
+  count = factory(Number(start_at_control.value), Number(step_control.value));
+
+  current_count.innerHTML = !isNaN(Number(start_at_control.value))
+    ? Number(start_at_control.value).toString()
+    : '0';
+}
 
 const start_at_control = document.getElementById(
-  "start_at",
+  'start_at',
 ) as HTMLInputElement;
 
-const step_control = document.getElementById("step") as HTMLInputElement;
+const step_control = document.getElementById('step') as HTMLInputElement;
 
-start_at_control?.addEventListener("change", () => {});
+start_at_control?.addEventListener('input', () => {
+  update_count_and_reset_counter();
+});
 
-step_control?.addEventListener("change", () => {});
+step_control?.addEventListener('input', () => {
+  update_count_and_reset_counter();
+});
 
 const count_button = document.querySelector(
-  ".count_button",
+  '.count_button',
 ) as HTMLButtonElement;
 
 const current_count = document.querySelector(
-  ".current_count",
+  '.current_count',
 ) as HTMLSpanElement;
 
-function update_count() {}
+function update_count() {
+  current_count.innerHTML = count().toString();
+}
 
-count_button.addEventListener("click", update_count);
+count_button.addEventListener('click', update_count);
